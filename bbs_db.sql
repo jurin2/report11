40bc368b503769e69652a67de43b9593b7482f19
jurin2 <89722981+jurin2@users.noreply.github.com>
2022-04-01 10:28:17 +0900

    Add files via upload

--- bbs_db.sql
+++ bbs_db.sql
@@ -6,14 +6,14 @@ create table border(
 	docNum int auto_increment,
     docTitle varchar(50) not null,
     docContent varchar(5000),
-    docDate date,
+    docDate date DEFAULT (CURRENT_DATE),
     primary key(docNum)
 );
 
-insert into border(docTitle,docContent,docDate) values('테스트1','온라인 게시판 내용보기 연습입니다. 테스트1',"2022-03-01");
-insert into border(docTitle,docContent,docDate) values('테스트2','온라인 게시판 내용보기 연습입니다. 테스트2',"2022-03-02");
-insert into border(docTitle,docContent,docDate) values('테스트3','온라인 게시판 내용보기 연습입니다. 테스트3',"2022-03-03");
-insert into border(docTitle,docContent,docDate) values('테스트4','온라인 게시판 내용보기 연습입니다. 테스트4',"2022-03-04");
-insert into border(docTitle,docContent,docDate) values('테스트5','온라인 게시판 내용보기 연습입니다. 테스트5',"2022-03-05");
+insert into border(docTitle,docContent) values('테스트1','온라인 게시판 내용보기 연습입니다. 테스트1');
+insert into border(docTitle,docContent) values('테스트2','온라인 게시판 내용보기 연습입니다. 테스트2');
+insert into border(docTitle,docContent) values('테스트3','온라인 게시판 내용보기 연습입니다. 테스트3');
+insert into border(docTitle,docContent) values('테스트4','온라인 게시판 내용보기 연습입니다. 테스트4');
+insert into border(docTitle,docContent) values('테스트5','온라인 게시판 내용보기 연습입니다. 테스트5');
 
 select * from border;
